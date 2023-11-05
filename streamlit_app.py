e53db1fd8b4ce2ac5f0f7055552fd3742813fecb
NASLINNEGOMO <149781676+NASLINNEGOMO@users.noreply.github.com>
2023-11-05 10:21:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit
-streamlit.title('My Mom's New Healthy Diner') 
+streamlit.title('My Moms New Healthy Diner') 
 
 streamlit.header(' Breakfast Favourites')
 streamlit.text('🥣 Omega 3 & Blueberry oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie ')
 streamlit.text('🐔 Hard_Boiled Free_Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
